5c22983f2659727a1a57a6e75e6d325c038ac645
Jesús Fco Cortés <jfcr@live.com>
2017-10-15 00:01:49 -0500

    Errase all inusefulli coponent and we are making de structure visual from the aplication
    TO DO: Make that ng serve run, there are a lot of problem an first we are going to make the login aplication logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { InventarioComponent } from './inventario/inventario.component';
-import { InventarioService }  from './inventario/inventario.service';
-import { VentasService } from './sale-point/ventas.service';
-import { CajaService } from './caja/caja.service';
+// import { InventarioComponent } from './inventario/inventario.component';
+// import { InventarioService }  from './inventario/inventario.service';
+// import { VentasService } from './sale-point/ventas.service';
+// import { CajaService } from './caja/caja.service';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
 //import { MaterializeModule } from 'materialize-css';
 
 import { NavegationComponent } from './navegation/navegation.component';
-import { InventarioDatailsComponent } from './inventario/inventario-datails/inventario-datails.component';
-import { InventarioListComponent } from './inventario/inventario-list/inventario-list.component';
-import { NewInventarioComponent } from './inventario/new-inventario/new-inventario.component';
-import { EditInventarioComponent } from './inventario/edit-inventario/edit-inventario.component';
-import { SalePointComponent } from './sale-point/sale-point.component';
-import { SaleListComponent } from './sale-point/sale-list/sale-list.component';
-import { SaleEditComponent } from './sale-point/sale-edit/sale-edit.component';
-import { SaleCreateComponent } from './sale-point/sale-create/sale-create.component';
-import { InventarioFilterPipe } from './inventario/inventario-list/inventario-filter.pipe';
-import { SugerenciasPipe } from './sale-point/sale-create/sugerencias.pipe';
-import { CajaComponent } from './caja/caja.component';
-import { LessProductComponent } from './less-product/less-product.component';
-import { AgregarInventarioComponent } from './inventario/agregar-inventario/agregar-inventario.component';
+// import { InventarioDatailsComponent } from './inventario/inventario-datails/inventario-datails.component';
+// import { InventarioListComponent } from './inventario/inventario-list/inventario-list.component';
+// import { NewInventarioComponent } from './inventario/new-inventario/new-inventario.component';
+// import { EditInventarioComponent } from './inventario/edit-inventario/edit-inventario.component';
+// import { SalePointComponent } from './sale-point/sale-point.component';
+// import { SaleListComponent } from './sale-point/sale-list/sale-list.component';
+// import { SaleEditComponent } from './sale-point/sale-edit/sale-edit.component';
+// import { SaleCreateComponent } from './sale-point/sale-create/sale-create.component';
+// import { InventarioFilterPipe } from './inventario/inventario-list/inventario-filter.pipe';
+// import { SugerenciasPipe } from './sale-point/sale-create/sugerencias.pipe';
+// import { CajaComponent } from './caja/caja.component';
+// import { LessProductComponent } from './less-product/less-product.component';
+// import { AgregarInventarioComponent } from './inventario/agregar-inventario/agregar-inventario.component';
 import { RegisterComponent } from './register/register.component';
 import { RegisterService } from './register/register.service';
 import { LoginComponent } from './login/login.component';
@@ -36,29 +36,28 @@ import { LoginService }  from './login/login.service';
 @NgModule({
   declarations: [
     AppComponent,
-    InventarioComponent,    
+    // InventarioComponent,    
     NavegationComponent,
-    InventarioDatailsComponent,
-    InventarioListComponent,
-    NewInventarioComponent,
-    EditInventarioComponent,
-    SalePointComponent,
-    SaleListComponent,
-    SaleEditComponent,
-    SaleCreateComponent,
-    InventarioFilterPipe,
-    SugerenciasPipe,
-    CajaComponent,
-    LessProductComponent,
-    AgregarInventarioComponent,
+    // InventarioDatailsComponent,
+    // InventarioListComponent,
+    // NewInventarioComponent,
+    // EditInventarioComponent,
+    // SalePointComponent,
+    // SaleListComponent,
+    // SaleEditComponent,
+    // SaleCreateComponent,
+    // InventarioFilterPipe,
+    // SugerenciasPipe,
+    // CajaComponent,
+    // LessProductComponent,
+    // AgregarInventarioComponent,
     RegisterComponent,
     LoginComponent,
     
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
+    FormsModule,    
     HttpModule,
     JsonpModule,
     //MaterializeModule,
@@ -70,7 +69,7 @@ import { LoginService }  from './login/login.service';
 //    ]),
     BrowserAnimationsModule,    
   ],
-  providers: [InventarioService, VentasService, CajaService, RegisterService , LoginService],
+  providers: [ RegisterService , LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/navegation/navegation.component.ts
+++ src/app/navegation/navegation.component.ts
@@ -1,9 +1,4 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { Inventario } from './../inventario/inventario';
-import { InventarioService } from './../inventario/inventario.service';
-import { CajaService } from '../caja/caja.service';
-import { Sale } from "./../sale-point/sale";
-
 
 @Component({
   selector: 'app-navegation',
@@ -12,118 +7,18 @@ import { Sale } from "./../sale-point/sale";
 })
 export class NavegationComponent implements OnInit {
     
-    salePointShow: boolean = true;
-    inventarioShow: boolean = false;
-    cajaShow: boolean = false;
-    lessProductoShow: boolean = false;  
-    showNotificaciones: boolean = false;
-    registerShow: boolean = false;
 
-    sale: Sale = new Sale();
-    sales: Array<Sale> = [];
 
-    caja: number;    
-    productos: Array<Inventario> = [];
+    
 
-    notificaciones: number = 0;
-
-    constructor(private _inventarioService: InventarioService,
-                private _cajaService: CajaService) { }            
+    constructor() { }            
 
     ngOnInit() {
-        this.getInventario();
-        this.getCaja();
-    }
-    
-    getInventario(){
-        this._inventarioService.getInventario()
-            .then(
-                inventario => this.productos.push(inventario),
-                error => this._inventarioService.errorArranque(error)
-            );
-    }
+        // this.getInventario();
+        // this.getCaja();
+    }       
 
-    getCaja() {
-
-        this._cajaService.getCaja()
-            .then(respCajaData => this.caja = respCajaData);
-    }
-    
-    showSalePoint(){
-        this.salePointShow = true;
-        this.inventarioShow = false;
-        this.cajaShow = false;
-        this.lessProductoShow = false;
-    }    
-
-    showInventario(){
-        this.salePointShow = false;
-        this.inventarioShow = true;                
-        this.cajaShow = false;
-        this.lessProductoShow = false;
-    }
-    showCaja(){
-        this.salePointShow = false;
-        this.inventarioShow = false;
-        this.cajaShow = true;
-        this.lessProductoShow = false;
-    }
-
-    showLessProducts() {
-        this.salePointShow = false;
-        this.inventarioShow = false;
-        this.cajaShow = false;
-        this.lessProductoShow = true;
-    }
-
-    showRegister(){
-        this.salePointShow = false;
-        this.inventarioShow = false;
-        this.cajaShow = false;
-        this.lessProductoShow = false; 
-        this.registerShow = true;
-    }
-    
-    venta(sale: Sale) {
-
-        this.caja += sale.total;
-        for (let articulo of sale.articulos){
-            for (let producto of this.productos) {
-
-                if (producto.id == articulo.producto_id){
-                    producto.inventario = producto.inventario - articulo.cantidad;
-                }
-
-                if (producto.inventario <= producto.reorden) {
-                    this.verificarInventarioAfterVenta(producto);
-                }
-            }
-        }
-    }
-
-    pushProductos(productos) {
-        this.productos = productos;  
-        this.verificarInventario();
-    }
-
-
-    pushNewProducto(producto: Inventario) {
-
-        console.log(producto);
-        this.productos.push(producto);
-
-        console.log(this.productos)
-    }
-
-    verificarInventario() {
-        for (let x of this.productos) {
-            if (x.inventario <= x.reorden) {                
-                this.notificaciones += 1;
-            }
-        }
-    }
-
-    verificarInventarioAfterVenta(producto: Inventario) {
+    cerrarSesion(){
 
     }
 }
